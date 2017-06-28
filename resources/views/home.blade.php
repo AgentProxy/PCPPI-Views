fd0fc1df252671c63bea30ea70d198c31905f52f
Lona Misa <rj.panaguiton10@gmail.com>
2017-06-28 13:25:22 +0800

    home-partial-view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,13 +10,11 @@
 <!-- Intro Section -->
     <section id="intro" class="intro-section">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1>Scrolling Nav</h1>
-                    <p><strong>Usage Instructions:</strong> Make sure to include the <code>scrolling-nav.js</code>, <code>jquery.easing.min.js</code>, and <code>scrolling-nav.css</code> files. To make a link smooth scroll to another section on the page, give the link the <code>.page-scroll</code> class and set the link target to a corresponding ID on the page.</p>
+            
+                
                     <a class="btn btn-default page-scroll" href="#about">Click Me to Scroll Down!</a>
-                </div>
-            </div>
+                
+           
         </div>
     </section>
 
@@ -24,8 +22,12 @@
     <section id="about" class="about-section">
         <div class="container">
             <div class="row">
-                <div class="col-lg-12">
-                    <h1>About Section</h1>
+                <div class="col-md-6">
+                    <h1>Why Join Pepsi?</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit in eros ut fermentum. Suspendisse eget dui dictum, mattis nisl hendrerit, consequat libero. Suspendisse vel rutrum sapien, ut congue leo. Sed porta, quam eget lacinia dictum, nunc purus mollis nunc, eu imperdiet felis ligula sit amet nibh. Nullam auctor dolor sit amet orci eleifend, in eleifend nibh gravida. Pellentesque mollis sed nisi sed suscipit. Suspendisse eu nisi a ligula ornare pulvinar. Curabitur lacinia ullamcorper eros, vitae dapibus orci iaculis eu. Aliquam tellus mi, gravida ac tristique sit amet, sagittis vel tortor. Etiam luctus mollis pharetra. Aliquam nec rutrum mi, ut maximus tellus. Sed pretium ante urna, at sodales nulla venenatis vel.</p>
+                </div>
+                <div class="col-md-6">
+                    
                 </div>
             </div>
         </div>
@@ -35,8 +37,12 @@
     <section id="services" class="services-section">
         <div class="container">
             <div class="row">
-                <div class="col-lg-12">
-                    <h1>Services Section</h1>
+                <div class="col-md-6">
+                    
+                </div>
+                <div class="col-md-6">
+                    <h1>Our Company</h1>                  
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit in eros ut fermentum. Suspendisse eget dui dictum, mattis nisl hendrerit, consequat libero. Suspendisse vel rutrum sapien, ut congue leo. Sed porta, quam eget lacinia dictum, nunc purus mollis nunc, eu imperdiet felis ligula sit amet nibh. Nullam auctor dolor sit amet orci eleifend, in eleifend nibh gravida. Pellentesque mollis sed nisi sed suscipit. Suspendisse eu nisi a ligula ornare pulvinar. Curabitur lacinia ullamcorper eros, vitae dapibus orci iaculis eu. Aliquam tellus mi, gravida ac tristique sit amet, sagittis vel tortor. Etiam luctus mollis pharetra. Aliquam nec rutrum mi, ut maximus tellus. Sed pretium ante urna, at sodales nulla venenatis vel.</p>
                 </div>
             </div>
         </div>
@@ -47,8 +53,24 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1>Contact Section</h1>
+                    <h1>Careers</h1>
                 </div>
+            </div>
+            <div class="row">
+                <a href="#">
+                <div class="col-md-4 col-md-1-offset col-md-offset-1 " style="border: 1px solid black; padding: 10px; border-radius: 20px; height: 418px;">
+                    <img src="/img/3.png" width="60%">
+                    <h1>Professionals</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit in eros ut fermentum. Suspendisse eget dui dictum, mattis nisl hendrerit, consequat libero. Suspendisse vel rutrum sapien, ut congue leo.</p>
+                </div>
+                </a>
+                <a href="#">
+                <div class="col-md-4 col-md-offset-2" style="border: 1px solid black; padding: 10px; border-radius: 20px; height: 40%; height: 418px;">
+                    <img src="/img/2.png" width="60%;">
+                    <h1>Internship</h1>
+                    <p>Curabitur lacinia ullamcorper eros, vitae dapibus orci iaculis eu. Aliquam tellus mi, gravida ac tristique sit amet, sagittis vel tortor. Etiam luctus mollis pharetra.</p>
+                </div>
+                </a>
             </div>
         </div>
     </section>
--- storage/framework/views/b4c45c2934506b578f9d0550091cf1681ab24dd1.php
+++ storage/framework/views/b4c45c2934506b578f9d0550091cf1681ab24dd1.php
@@ -1,10 +1,76 @@
 <?php $__env->startSection('content'); ?>
-<div class="container">
+<!-- <div class="container">
 	<div class="row">
-		<div class="col-md-8 col-md-offset-2">
-		<h1> PCPPI </h1>
-		</div>
+		
 	</div>
-</div>
+</div> -->
+
+<!-- Intro Section -->
+    <section id="intro" class="intro-section">
+        <div class="container">
+            
+                
+                    <a class="btn btn-default page-scroll" href="#about">Click Me to Scroll Down!</a>
+                
+           
+        </div>
+    </section>
+
+    <!-- About Section -->
+    <section id="about" class="about-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h1>Why Join Pepsi?</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit in eros ut fermentum. Suspendisse eget dui dictum, mattis nisl hendrerit, consequat libero. Suspendisse vel rutrum sapien, ut congue leo. Sed porta, quam eget lacinia dictum, nunc purus mollis nunc, eu imperdiet felis ligula sit amet nibh. Nullam auctor dolor sit amet orci eleifend, in eleifend nibh gravida. Pellentesque mollis sed nisi sed suscipit. Suspendisse eu nisi a ligula ornare pulvinar. Curabitur lacinia ullamcorper eros, vitae dapibus orci iaculis eu. Aliquam tellus mi, gravida ac tristique sit amet, sagittis vel tortor. Etiam luctus mollis pharetra. Aliquam nec rutrum mi, ut maximus tellus. Sed pretium ante urna, at sodales nulla venenatis vel.</p>
+                </div>
+                <div class="col-md-6">
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Services Section -->
+    <section id="services" class="services-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    
+                </div>
+                <div class="col-md-6">
+                    <h1>Our Company</h1>                  
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit in eros ut fermentum. Suspendisse eget dui dictum, mattis nisl hendrerit, consequat libero. Suspendisse vel rutrum sapien, ut congue leo. Sed porta, quam eget lacinia dictum, nunc purus mollis nunc, eu imperdiet felis ligula sit amet nibh. Nullam auctor dolor sit amet orci eleifend, in eleifend nibh gravida. Pellentesque mollis sed nisi sed suscipit. Suspendisse eu nisi a ligula ornare pulvinar. Curabitur lacinia ullamcorper eros, vitae dapibus orci iaculis eu. Aliquam tellus mi, gravida ac tristique sit amet, sagittis vel tortor. Etiam luctus mollis pharetra. Aliquam nec rutrum mi, ut maximus tellus. Sed pretium ante urna, at sodales nulla venenatis vel.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact Section -->
+    <section id="contact" class="contact-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Careers</h1>
+                </div>
+            </div>
+            <div class="row">
+                <a href="#">
+                <div class="col-md-4 col-md-1-offset col-md-offset-1 " style="border: 1px solid black; padding: 10px; border-radius: 20px; height: 418px;">
+                    <img src="/img/3.png" width="60%">
+                    <h1>Professionals</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit in eros ut fermentum. Suspendisse eget dui dictum, mattis nisl hendrerit, consequat libero. Suspendisse vel rutrum sapien, ut congue leo.</p>
+                </div>
+                </a>
+                <a href="#">
+                <div class="col-md-4 col-md-offset-2" style="border: 1px solid black; padding: 10px; border-radius: 20px; height: 40%; height: 418px;">
+                    <img src="/img/2.png" width="60%;">
+                    <h1>Internship</h1>
+                    <p>Curabitur lacinia ullamcorper eros, vitae dapibus orci iaculis eu. Aliquam tellus mi, gravida ac tristique sit amet, sagittis vel tortor. Etiam luctus mollis pharetra.</p>
+                </div>
+                </a>
+            </div>
+        </div>
+    </section>
 <?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.header', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+<?php echo $__env->make('layouts.master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
