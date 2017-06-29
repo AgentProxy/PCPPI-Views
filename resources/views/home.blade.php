558bb5839fad30c1260ea66d57e7d10734c889c6
Lona Misa <rj.panaguiton10@gmail.com>
2017-06-29 08:42:48 +0800

    06-2917

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,59 +9,70 @@
 
 <!-- Intro Section -->
 
-    <header class="w3-display-container w3-content w3-center" style="max-width:1500px">
-  <img class="w3-image" src="img/HOMEbg-1.jpg" alt="Me" width="1500" height="600">
-  
-  
-  <!-- Navbar (placed at the bottom of the header image) -->
-  <div class="w3-bar w3-light-grey w3-round w3-display-bottommiddle w3-hide-small" style="bottom:46px">
-    <a class="btn btn-default page-scroll" href="#about">Click Me to Scroll Down!</a>
-  </div>
-</header>
+    <section id="intro" class="intro-section">
+        <div class="container">
+            
+                
+            <a class="btn btn-default page-scroll" href="#about">Click Me to Scroll Down!</a>
+                
+           
+        </div>
+    </section>
 
-
-</div>
-<!-- Page content -->
-<div id="about">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <h1>Why Join Pepsi?</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit in eros ut fermentum. Suspendisse eget dui dictum, mattis nisl hendrerit, consequat libero. Suspendisse vel rutrum sapien, ut congue leo. Sed porta, quam eget lacinia dictum, nunc purus mollis nunc, eu imperdiet felis ligula sit amet nibh. Nullam auctor dolor sit amet orci eleifend, in eleifend nibh gravida. Pellentesque mollis sed nisi sed suscipit. Suspendisse eu nisi a ligula ornare pulvinar. Curabitur lacinia ullamcorper eros, vitae dapibus orci iaculis eu. Aliquam tellus mi, gravida ac tristique sit amet, sagittis vel tortor. Etiam luctus mollis pharetra. Aliquam nec rutrum mi, ut maximus tellus. Sed pretium ante urna, at sodales nulla venenatis vel.</p>
+    <!-- About Section -->
+    <section id="about" class="about-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h1>Why Join Pepsi?</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit in eros ut fermentum. Suspendisse eget dui dictum, mattis nisl hendrerit, consequat libero. Suspendisse vel rutrum sapien, ut congue leo. Sed porta, quam eget lacinia dictum, nunc purus mollis nunc, eu imperdiet felis ligula sit amet nibh. Nullam auctor dolor sit amet orci eleifend, in eleifend nibh gravida. Pellentesque mollis sed nisi sed suscipit. Suspendisse eu nisi a ligula ornare pulvinar. Curabitur lacinia ullamcorper eros, vitae dapibus orci iaculis eu. Aliquam tellus mi, gravida ac tristique sit amet, sagittis vel tortor. Etiam luctus mollis pharetra. Aliquam nec rutrum mi, ut maximus tellus. Sed pretium ante urna, at sodales nulla venenatis vel.</p>
                 </div>
-            <div class="col-md-6">
+                <div class="col-md-6">
                     
+                </div>
             </div>
         </div>
-    </div>
-</div>
-<div>
-    <div class="container">
-        <div class="row">
-        <div class="col-md-6">
+    </section>
+
+    <!-- Services Section -->
+    <section id="services" class="services-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
                     
-        </div>
-        <div class="col-md-6">
-            <h1>Our Company</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit in eros ut fermentum. Suspendisse eget dui dictum, mattis nisl hendrerit, consequat libero. Suspendisse vel rutrum sapien, ut congue leo. Sed porta, quam eget lacinia dictum, nunc purus mollis nunc, eu imperdiet felis ligula sit amet nibh. Nullam auctor dolor sit amet orci eleifend, in eleifend nibh gravida. Pellentesque mollis sed nisi sed suscipit. Suspendisse eu nisi a ligula ornare pulvinar. Curabitur lacinia ullamcorper eros, vitae dapibus orci iaculis eu. Aliquam tellus mi, gravida ac tristique sit amet, sagittis vel tortor. Etiam luctus mollis pharetra. Aliquam nec rutrum mi, ut maximus tellus. Sed pretium ante urna, at sodales nulla venenatis vel.</p>
-        </div>       
-        </div>
-    </div>
-</div>
-<div>
-    <div class="container">
-        <h1>Careers</h1>
-        <div class="row">
-            <div class="col-md-4 col-md-offset-1">
-                <h1>Professionals</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit in eros ut fermentum. Suspendisse eget dui dictum, mattis nisl hendrerit, consequat libero. Suspendisse vel rutrum sapien, ut congue leo. Sed porta, quam eget lacinia dictum, nunc purus mollis nunc, eu imperdiet felis ligula sit amet nibh. Nullam auctor dolor sit amet orci eleifend, in eleifend nibh gravida.</p>
-            </div>
-            <div class="col-md-4 col-md-offset-2">
-                <h1>Internship</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit in eros ut fermentum. Suspendisse eget dui dictum, mattis nisl hendrerit, consequat libero. Suspendisse vel rutrum sapien, ut congue leo. Sed porta, quam eget lacinia dictum, nunc purus mollis nunc, eu imperdiet felis ligula sit amet nibh. Nullam auctor dolor sit amet orci eleifend, in eleifend nibh gravida.</p>
+                </div>
+                <div class="col-md-6">
+                    <h1>Our Company</h1>                  
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit in eros ut fermentum. Suspendisse eget dui dictum, mattis nisl hendrerit, consequat libero. Suspendisse vel rutrum sapien, ut congue leo. Sed porta, quam eget lacinia dictum, nunc purus mollis nunc, eu imperdiet felis ligula sit amet nibh. Nullam auctor dolor sit amet orci eleifend, in eleifend nibh gravida. Pellentesque mollis sed nisi sed suscipit. Suspendisse eu nisi a ligula ornare pulvinar. Curabitur lacinia ullamcorper eros, vitae dapibus orci iaculis eu. Aliquam tellus mi, gravida ac tristique sit amet, sagittis vel tortor. Etiam luctus mollis pharetra. Aliquam nec rutrum mi, ut maximus tellus. Sed pretium ante urna, at sodales nulla venenatis vel.</p>
+                </div>
             </div>
         </div>
-    </div>
-</div>
+    </section>
+
+    <!-- Contact Section -->
+    <section id="contact" class="contact-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1>Careers</h1>
+                </div>
+            </div>
+            <div class="row">
+                <a href="#">
+                <div class="col-md-4 col-md-1-offset" style="border: 1px solid black; padding: 10px; border-radius: 20px;">
+                    <img src="/img/3.png" width="60%">
+                    <h1>Professionals</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit in eros ut fermentum. Suspendisse eget dui dictum, mattis nisl hendrerit, consequat libero. Suspendisse vel rutrum sapien, ut congue leo.</p>
+                </div>
+                </a>
+                <a href="#">
+                <div class="col-md-4 col-md-offset-2" style="border: 1px solid black; padding: 10px; border-radius: 20px; height: 40%;">
+                    <img src="/img/2.png" width="60%;">
+                    <h1>Internship</h1>
+                    <p>Curabitur lacinia ullamcorper eros, vitae dapibus orci iaculis eu. Aliquam tellus mi, gravida ac tristique sit amet, sagittis vel tortor. Etiam luctus mollis pharetra.</p>
+                </div>
+                </a>
+            </div>
+        </div>
     </section>
 @endsection
