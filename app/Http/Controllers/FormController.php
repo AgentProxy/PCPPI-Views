15f14f22898a854e2ee0099bfacf8c9b40d52d1a
Eric Joseph Flores <ericjoseph.flores1@gmail.com>
2017-07-05 11:53:12 +0800

    Form Validation unfinished and Recaptcha Update

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -12,60 +12,60 @@ use Illuminate\Routing\Controller as BaseController;
 class FormController extends Controller
 {
     function sendApplication(Request $request, $form_type){
-        $this->validate($request, [
-            'g-recaptcha-response' => 'required|recaptcha',
-            'fname' => 'required|max:75',
-            'lname' => 'required|max:75',
-            'present' => 'required|max:150',
-            'zip' => 'max:6',
-            'prov' => 'required|max:150',
-            'zip1' => 'max:6',
-            'bday'=> 'required|date',
-            'phone'=> 'required|max:11',
-            'email'=> 'required|email|max:75',
-            'course'=>'required|max:75',
-            'from1'=>'required',
-            'to1'=>'required',
-            'school1'=>'required|max:75',
-            'gradcourse'=>'max:75',
-            'school2'=>'max:75',
-            //from and to grad course
-            //skills 
-            'name1'=>'required|max:75',
-            'num1'=>'required|max:11',
-            'rel1'=>'required|max:75',
-            'name2'=>'required|max:75',
-            'num2'=>'required|max:11',
-            'rel2'=>'required|max:75',
-            'name3'=>'required|max:75',
-            'num3'=>'required|max:11',
-            'rel3'=>'required|max:75',
-            //companies
-            'reloc',=>'required',
-            'resume'=>'required|file|mimes:doc,pdf,docx|max:2097152'
-            /*
-            //for interns 
-            'school' => 'required|max:75',
-            'course'=> 'required|max:75',
-            'level' => 'required',
-            'sem' => 'required',
-            'hrs' => 'required',
+        // $this->validate($request, [
+        //     'g-recaptcha-response' => 'required|recaptcha',
+        //     'fname' => 'required|max:75',
+        //     'lname' => 'required|max:75',
+        //     'present' => 'required|max:150',
+        //     'zip' => 'max:6',
+        //     'prov' => 'required|max:150',
+        //     'zip1' => 'max:6',
+        //     'bday'=> 'required|date',
+        //     'phone'=> 'required|max:11',
+        //     'email'=> 'required|email|max:75',
+        //     'course'=>'required|max:75',
+        //     'from1'=>'required',
+        //     'to1'=>'required',
+        //     'school1'=>'required|max:75',
+        //     'gradcourse'=>'max:75',
+        //     'school2'=>'max:75',
+        //     //from and to grad course
+        //     //skills 
+        //     'name1'=>'required|max:75',
+        //     'num1'=>'required|max:11',
+        //     'rel1'=>'required|max:75',
+        //     'name2'=>'required|max:75',
+        //     'num2'=>'required|max:11',
+        //     'rel2'=>'required|max:75',
+        //     'name3'=>'required|max:75',
+        //     'num3'=>'required|max:11',
+        //     'rel3'=>'required|max:75',
+        //     //companies
+        //     'reloc',=>'required',
+        //     'resume'=>'required|file|mimes:doc,pdf,docx|max:2097152'
+        //     /*
+        //     //for interns 
+        //     'school' => 'required|max:75',
+        //     'course'=> 'required|max:75',
+        //     'level' => 'required',
+        //     'sem' => 'required',
+        //     'hrs' => 'required',
         
-            //for bank forms
-            'dept' => 'required',
-            'loc' => 'required',
-            'reloc' => 'required',
+        //     //for bank forms
+        //     'dept' => 'required',
+        //     'loc' => 'required',
+        //     'reloc' => 'required',
 
-            //for skills
-            foreach($this->request->get('items') as $key => $val)
-              {
-                $rules['items.'.$key] = 'required|max:10';
-              }
-            //
+        //     //for skills
+        //     foreach($this->request->get('items') as $key => $val)
+        //       {
+        //         $rules['items.'.$key] = 'required|max:10';
+        //       }
+        //     //
         
 
-            */
-        ]);
+        //     */
+        // ]);
 
         $data = array();
         $data_uncleaned = $request->all();
