58924a833e2e2590eaef9469df4f13b24b65a131
Eric Joseph Flores <ericjoseph.flores1@gmail.com>
2017-07-19 09:19:36 +0800

    Map IDs Changed

--- storage/framework/views/7bd77fab8370533d09a256fc910ae2ede90e0339.php
+++ storage/framework/views/7bd77fab8370533d09a256fc910ae2ede90e0339.php
@@ -1,4 +1,4 @@
-<textarea class="form-control" name="<?php echo e($row->field); ?>" id="<?php echo e($row->field); ?>"><?php if(isset($dataTypeContent->{$row->field})): ?><?php echo e(old($row->field, $dataTypeContent->{$row->field})); ?><?php elseif(isset($options->default)): ?><?php echo e(old($row->field, $options->default)); ?><?php else: ?><?php echo e(old($row->field)); ?><?php endif; ?></textarea>
+<textarea class="form-control" style="height: 200px;" name="<?php echo e($row->field); ?>" id="<?php echo e($row->field); ?>"><?php if(isset($dataTypeContent->{$row->field})): ?><?php echo e(old($row->field, $dataTypeContent->{$row->field})); ?><?php elseif(isset($options->default)): ?><?php echo e(old($row->field, $options->default)); ?><?php else: ?><?php echo e(old($row->field)); ?><?php endif; ?></textarea>
 
 <script>
 
--- storage/framework/views/d4a92f23a4fd98750d5a378dc255a9d778ce3004.php
+++ storage/framework/views/d4a92f23a4fd98750d5a378dc255a9d778ce3004.php
@@ -1,6 +1,4 @@
-@extends('layouts.master')
-
-@section('content')
+<?php $__env->startSection('content'); ?>
 		<h1 class="text-center"> Find Your Career </h1>
         <form method="GET" action="/careers-search">
             <div id="search-bar" class="container">
@@ -10,9 +8,9 @@
                 <div class="form-group col-md-4">
                     <select class="form-control" name="region" id="region">
                         <option value="0">All Regions</option>
-                        @foreach($regions as $region)
-                        <option value="{{$region->id}}">{{$region->name}}</option>
-                        @endforeach
+                        <?php $__currentLoopData = $regions; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $region): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                        <option value="<?php echo e($region->id); ?>"><?php echo e($region->name); ?></option>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                     </select>
                 </div>
                 <div class="col-md-4" id="search">
@@ -28,7 +26,10 @@
         map = new google.maps.Map(document.getElementById('map'), { 	
 	        	center: philippines,
 	        	zoom: 6
+                // disableDoubleClickZoom: true
         });
+
+        // setMarkers(map);
 
         var regions = [
         	['Cagayan Valley Operations',16.9679036,121.77049050000005,0],
@@ -51,25 +52,25 @@
         ];
 
         var contents = [
-        	'<h4> Cagayan Valley Operations </h4><a href="/careers-search/1"><h5>{{count($vacancies->where("region_id",1))}} Vacancies</h5></a>',
-        	'<h4> North Luzon Operations </h4><a href="/careers-search/2"><h5>{{count($vacancies->where("region_id",2))}} Vacancies</h5></a>',
-        	'<h4> Central Luzon </h4><a href="/careers-search/3"><h5>{{count($vacancies->where("region_id",3))}} Vacancies</h5></a>',
-        	'<h4> Modern Trade Operations </h4><a href="/careers-search/4"><h5>{{count($vacancies->where("region_id",4))}} Vacancies</h5></a>',
+        	'<h4> Cagayan Valley Operations </h4><a href="/careers-search/1"><h5><?php echo e(count($vacancies->where("region_id",1))); ?> Vacancies</h5></a>',
+        	'<h4> North Luzon Operations </h4><a href="/careers-search/2"><h5><?php echo e(count($vacancies->where("region_id",2))); ?> Vacancies</h5></a>',
+        	'<h4> Central Luzon </h4><a href="/careers-search/3"><h5><?php echo e(count($vacancies->where("region_id",3))); ?> Vacancies</h5></a>',
+        	'<h4> Modern Trade Operations </h4><a href="/careers-search/4"><h5><?php echo e(count($vacancies->where("region_id",4))); ?> Vacancies</h5></a>',
 
-        	'<h4> Metro Operations </h4><a href="/careers-search/5"><h5>{{count($vacancies->where("region_id",5))}} Vacancies</h5></a> <hr/> <h4> Head Office </h4><a href="/careers-search/6"><h5>{{count($vacancies->where("region_id",6))}} Vacancies</h5></a> <hr /> <h4> Metro Sales </h4><a href="/careers-search/7"><h5>{{count($vacancies->where("region_id",7))}} Vacancies</h5></a>',
+        	'<h4> Metro Operations </h4><a href="/careers-search/5"><h5><?php echo e(count($vacancies->where("region_id",5))); ?> Vacancies</h5></a> <hr/> <h4> Head Office </h4><a href="/careers-search/6"><h5><?php echo e(count($vacancies->where("region_id",6))); ?> Vacancies</h5></a> <hr /> <h4> Metro Sales </h4><a href="/careers-search/7"><h5><?php echo e(count($vacancies->where("region_id",7))); ?> Vacancies</h5></a>',
 
-            // '<h4> Head Office </h4><a href="/careers-search/6"><h5>{{count($vacancies->where("region_id",8))}} Vacancies</h5></a>',
-            // '<h4> Metro Sales </h4><a href="/careers-search/7"><h5>{{count($vacancies->where("region_id",9))}} Vacancies</h5></a>',
-        	'<h4> Southern Tagalog Region Operations </h4><a href="/careers-search/8"><h5>{{count($vacancies->where("region_id",8))}} Vacancies</h5></a>',
-        	'<h4> Bicol Region Operations</h4><a href="/careers-search/9"><h5>{{count($vacancies->where("region_id",9))}} Vacancies</h5></a>',
-        	'<h4> Iloilo </h4><a href="/careers-search/10"><h5>{{count($vacancies->where("region_id",10))}} Vacancies</h5></a>',
-        	'<h4> Tanuan </h4><a href="/careers-search/11"><h5>{{count($vacancies->where("region_id",11))}} Vacancies</h5></a>',
-        	'<h4> Cebu </h4><a href="/careers-search/12"><h5>{{count($vacancies->where("region_id",12))}} Vacancies</h5></a>',
-        	'<h4> Bacolod </h4><a href="/careers-search/13"><h5>{{count($vacancies->where("region_id",13))}} Vacancies</h5></a>',
-        	'<h4> Cagayan De Oro </h4><a href="/careers-search/14"><h5>{{count($vacancies->where("region_id",14))}} Vacancies</h5></a>',
-        	'<h4> Zamboanga </h4><a href="/careers-search/15"><h5>{{count($vacancies->where("region_id",15))}} Vacancies</h5></a>',
-        	'<h4> Davao </h4><a href="/careers-search/16"><h5>{{count($vacancies->where("region_id",16))}} Vacancies</h5></a>',
-            '<h4> Cabuyao </h4><a href="/careers-search/17"><h5>{{count($vacancies->where("region_id",17))}} Vacancies</h5></a>'
+            // '<h4> Head Office </h4><a href="/careers-search/6"><h5><?php echo e(count($vacancies->where("region_id",8))); ?> Vacancies</h5></a>',
+            // '<h4> Metro Sales </h4><a href="/careers-search/7"><h5><?php echo e(count($vacancies->where("region_id",9))); ?> Vacancies</h5></a>',
+        	'<h4> Southern Tagalog Region Operations </h4><a href="/careers-search/8"><h5><?php echo e(count($vacancies->where("region_id",8))); ?> Vacancies</h5></a>',
+        	'<h4> Bicol Region Operations</h4><a href="/careers-search/9"><h5><?php echo e(count($vacancies->where("region_id",9))); ?> Vacancies</h5></a>',
+        	'<h4> Iloilo </h4><a href="/careers-search/10"><h5><?php echo e(count($vacancies->where("region_id",10))); ?> Vacancies</h5></a>',
+        	'<h4> Tanuan </h4><a href="/careers-search/11"><h5><?php echo e(count($vacancies->where("region_id",11))); ?> Vacancies</h5></a>',
+        	'<h4> Cebu </h4><a href="/careers-search/12"><h5><?php echo e(count($vacancies->where("region_id",12))); ?> Vacancies</h5></a>',
+        	'<h4> Bacolod </h4><a href="/careers-search/13"><h5><?php echo e(count($vacancies->where("region_id",13))); ?> Vacancies</h5></a>',
+        	'<h4> Cagayan De Oro </h4><a href="/careers-search/14"><h5><?php echo e(count($vacancies->where("region_id",14))); ?> Vacancies</h5></a>',
+        	'<h4> Zamboanga </h4><a href="/careers-search/15"><h5><?php echo e(count($vacancies->where("region_id",15))); ?> Vacancies</h5></a>',
+        	'<h4> Davao </h4><a href="/careers-search/16"><h5><?php echo e(count($vacancies->where("region_id",16))); ?> Vacancies</h5></a>',
+            '<h4> Cabuyao </h4><a href="/careers-search/17"><h5><?php echo e(count($vacancies->where("region_id",17))); ?> Vacancies</h5></a>'
         ];
 
         // var regions = [
@@ -140,4 +141,5 @@
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC7G6W1Bacdwk-cXeQMnwVy31foc802U2w&callback=initMap"
     async defer></script>
 
-@endsection
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
