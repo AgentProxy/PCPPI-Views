7e835c427febcc9b88367156be2a014d931df7dd
Eric Joseph Flores <ericjoseph.flores1@gmail.com>
2017-07-25 15:31:24 +0800

    Sequence of searchboxes arranged and Leave Resume Label is moved downwards

--- storage/framework/views/af77b55e915537e8289ea6fe88c908f68e10ead1.php
+++ storage/framework/views/af77b55e915537e8289ea6fe88c908f68e10ead1.php
@@ -1,14 +1,11 @@
 <?php $__env->startSection('content'); ?>
-<div id="section" class="container">
-    <div id="search-header" >
+<div id="section" class="container bg-grey last">
+    <div id="search-header">
         <div class="row">
             <h3 class="bold">FIND YOUR CAREER</h3>
         </div>
-        <div class="row">
-            <form method="GET" action="/careers-retrieve">
-                <div class="form-group col-md-3">
-                    <input type="text" class="form-control" name="job" id="job" placeholder="Job" value="<?php echo old('job'); ?>">
-                </div>
+        <form method="GET" action="/careers-retrieve">
+            <div class="row">
                 <div class="form-group col-md-3">
                     <select class="form-control" id="region" name="region">
                         <option value="0" >All Regions</option>
@@ -25,16 +22,16 @@
                         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
                     </select>
                 </div>
+                <div class="form-group col-md-3">
+                    <input type="text" class="form-control" name="job" id="job" placeholder="Job" value="<?php echo old('job'); ?>">
+                </div>
                 <div class="col-md-3">
                     <input type="submit" class="btn btn-primary col-md-12" value="Search" />
                 </div>
-            </form>
-        </div>
-        <div class="row" id="padbot">
-            <h4>Can't find any vacancy in desired region and position? Leave your resume <a id="bank" href="/careers-bankform"> here</a>.  </h4>
-        </div>
+            </div>
+        </form>
     </div>
-    <div id="search-results" class="full-height" style="width: 100%; min-height: 200px; background-color: white;">
+    <div id="search-results" class="full-height" style="width: 100%; min-height: 200px;">
         <?php $__empty_1 = true; $__currentLoopData = $vacancies; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $vacancy): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); $__empty_1 = false; ?>
         <div class="panel panel-default"> 
             <div class="panel-heading">
@@ -48,17 +45,20 @@
                         <h4><?php echo e($vacancy->description); ?></h4>
                     </div>
                     <div class="col-md-2" style="padding-top: 15px;">
-                        <a href="careers-proform/<?php echo e($vacancy->id); ?>" class="btn btn-view btn-cherry btn-lg col-xs-offset-1" role="button">View Details</a>
+                        <a href="/careers-proform/<?php echo e($vacancy->id); ?>" class="btn btn-view btn-lg col-xs-offset-1" role="button">View Details</a>
                     </div>
                 </div>
             </div>
         </div>
         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); if ($__empty_1): ?>
-        <h1 style="text-align: center; padding-top: 8%"> No Vacancies Found </h1>
+        <h1 style="text-align: center; padding-top: 8%;"> No Vacancies Found </h1>
         <?php endif; ?>
     </div>
     <div style="text-align: center;">
         <div class="pagination" style="margin: auto;"> <?php echo e($vacancies->links()); ?> </div>
+    </div>
+    <div class="row" id="padbot" style="text-align: center">
+        <h4>Can't find any vacancy in desired region and position? Leave your resume <a id="bank" href="/careers-bankform"> here</a>.  </h4>
     </div>
 </div>
 <?php $__env->stopSection(); ?>
